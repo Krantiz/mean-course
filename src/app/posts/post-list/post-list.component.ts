7b26cea25c4434db3ec62ede0f735cdd2bf43242
Krantikumar <krantikumar@greatplaceitservices.com>
2018-09-19 10:13:39 +0530

    Started creating Attendence tracker

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  posts = [
+  	{'empName': 'emp1', 'status': 'present', 'remark': 'Remark1'},
+  	{'empName': 'emp2', 'status': 'present', 'remark': 'Remark2'},
+  	{'empName': 'emp3', 'status': 'present', 'remark': 'Remark3'}
+  ]
+}
